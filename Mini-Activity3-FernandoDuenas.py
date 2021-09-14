98cebfba5aca21bc78256315bcb85fb9b08a7178
Fernando Duenas <fernandoduenas@Fernandos-MBP.lan>
2021-09-14 11:51:33 -0400

    first commit

---
+++ Mini-Activity3-FernandoDuenas.py
@@ -0,0 +1,13 @@
+ToDoList = ["Pay Bills", "File Taxes", "Research", "Sleep", "Eat"]
+print(ToDoList)
+
+# Change the first item in the list to "Travel"
+ToDoList[0] = "Travel"
+print(ToDoList)
+
+# Change the second to last item in the list to "Exercise"
+ToDoList[-2] = "Exercise"
+print(ToDoList)
+
+# Print the Original To Do List, and the Modified To Do List.
+print(ToDoList)
